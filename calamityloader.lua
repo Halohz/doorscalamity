9caee9775f926ab475f18969a83965bf60948854
Halohz <uhalozh@gmail.com>
2024-09-09 22:17:45 +0100

    Update calamityloader.lua

--- calamityloader.lua
+++ calamityloader.lua
@@ -9,36 +9,36 @@ then
     local IMAGE = "rbxassetid://13429676701"
     local Positions = UDim2.new(0.001, 0, 0.54, 0)
     local Sizes = UDim2.new(0, 76, 0, 70)
- 
+
     -- Create GUI elements
     local CalamityGUI = Instance.new("ScreenGui")
     local Frame = Instance.new("Frame")
     local ToggleButton = Instance.new("ImageButton")
- 
+
     -- Configure GUI properties
     CalamityGUI.Name = "CalamityOpen"
     CalamityGUI.Parent = game:WaitForChild("CoreGui")
     CalamityGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
- 
+
     Frame.Name = "100x100"
     Frame.Parent = CalamityGUI
     Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     Frame.Position = Positions
     Frame.Size = UDim2.new(0, 50, 0, 45)
- 
+
     ToggleButton.Parent = Frame
     ToggleButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     ToggleButton.Size = Sizes
     ToggleButton.Image = IMAGE
- 
+
     -- Toggle function for enabling/disabling the Calamity GUI
     ToggleButton.MouseButton1Down:Connect(function()
         local calamityGui = game.CoreGui:FindFirstChild("CalamityHub")
         if calamityGui then
             calamityGui.Enabled = not calamityGui.Enabled
+        else
+            -- Load the Calamity script using the Orion UI Library
+            loadstring(game:HttpGet('https://pastebin.com/raw/z0EHQ8ZA'))()
         end
     end)
- 
-    -- Load the Calamity script from Pastebin
-    loadstring(game:HttpGet("https://pastebin.com/raw/z0EHQ8ZA"))()
 end
